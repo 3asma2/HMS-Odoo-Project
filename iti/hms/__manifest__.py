734ed03bd7da757b23836c57171e18d069d9f48a
asmaa <3asmaa.alaa@gmail.com>
2022-02-16 21:40:36 +0200

    adding hms

--- iti/hms/__manifest__.py
+++ iti/hms/__manifest__.py
@@ -3,11 +3,15 @@
     'summary':'this moduel for hospitals',
 
     'data':[
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/hms_view.xml',
         'views/hms_department_view.xml',
         'views/hms_doctor_view.xml',
         'views/log_history_view.xml',
-        'views/crm_customer_view.xml'
+        'views/crm_customer_view.xml',
+        'reports/report.xml',
+        'reports/template.xml',
     ],
     'depends':['crm'],
 }
